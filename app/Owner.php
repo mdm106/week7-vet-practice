7beaac141a5d1f96ab785f205605341f907f65c6
mdm106 <mads.mcmahon@googlemail.com>
2020-05-12 16:58:23 +0100

    Data for 5 owners added to 'owner' database, and methods to return fullname, fulladdress and to format telephone number added to 'owner' class

--- app/Owner.php
+++ app/Owner.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
 {
-    //
+    //method to get fullName
+    public function fullName() : string
+    {
+        return $this->first_name . " " . $this->last_name;
+    }
+
+    //method to display fullAddress
+    public function fullAddress() : string 
+    {
+       return  "{$this->address_1}, {$this->address_2}, {$this->town}, {$this->postcode}";
+    }
+
+    public function formattedPhoneNumber() : Owner
+    {
+        $this->telephone = substr($this->telephone, 0, 4) . " " . substr($this->telephone, 4);
+        $this->save();
+        return $this;
+    }
 }

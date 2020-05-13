eb56b3a28b7504e7b8c0c00cda0b1e6381e22448
mdm106 <mads.mcmahon@googlemail.com>
2020-05-13 13:17:52 +0100

    Adding formatTelephone method to owner_list.blade.php

--- resources/views/_parts/owner_list.blade.php
+++ resources/views/_parts/owner_list.blade.php
@@ -2,5 +2,7 @@
     <h4 class="font-weight-normal mt-3">Our valued owners:</h4>
     @foreach (App\Owner::all() as $owner)
     <p class="font-weight-light mt-3">{{ $owner->fullName() }}</p>
+    <p class="font-weight-light mt-3">{{ $owner->fullAddress() }}</p>
+    <p class="font-weight-light mt-3">{{ $owner->formatTelephone() }}</p>
     @endforeach
 </div>

8a807f44d8ac83417f7bbf5aeacea52e514c71c0
mdm106 <mads.mcmahon@googlemail.com>
2020-05-13 14:41:42 +0100

    Added owners.blade.php which uses owner_list. Added owners controller which passes through the owners database. Logic added to owners.blade.php to show message if no owners found

---
+++ resources/views/owners.blade.php
@@ -0,0 +1,14 @@
+@extends('app')
+@section('title'){{
+    'MDM Veterinary Practice - Owners'
+}}@endsection
+@section('content')
+    <div class="container">
+    @if(count($owners) == 0) 
+        <h3 class="mt-3">No owners found</h3>
+    @else 
+        <h3 class="mt-3">Our valued customers:</h3>
+        @include('_parts/owner_list')
+    @endif
+    </div>
+@endsection

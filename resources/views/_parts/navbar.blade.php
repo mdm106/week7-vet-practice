4cb0d25dc7c9f1bf94c27f33a2f362fbe35b8f1b
mdm106 <mads.mcmahon@googlemail.com>
2020-05-13 12:12:06 +0100

    Styling added to footer and nav

--- resources/views/_parts/navbar.blade.php
+++ resources/views/_parts/navbar.blade.php
@@ -1,4 +1,4 @@
- <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+ <nav id="override" class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand" href="#">MDM Vets</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse">
     <ul class="navbar-nav mr-auto w-100">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
+        <a id="override" class="nav-link" href="#">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">About us</a>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,12 +10,10 @@
   <link rel="manifest" href="site.webmanifest">
   <link rel="apple-touch-icon" href="icon.png">
   <!-- Place favicon.ico in the root directory -->
-
-  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-
   <meta name="theme-color" content="#fafafa">
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 
 <body>

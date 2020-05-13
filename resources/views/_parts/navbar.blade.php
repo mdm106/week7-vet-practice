a4d2acbedad1b914f658e69cebd53b377ef9966b
mdm106 <mads.mcmahon@googlemail.com>
2020-05-13 11:20:05 +0100

    Adding navbar content including div for ul of nav links and button for nav-brand, and styling added

--- resources/views/_parts/navbar.blade.php
+++ resources/views/_parts/navbar.blade.php
@@ -1,6 +1,23 @@
- <nav class="navbar">
-          <a class="navbar-brand" href="#">Home</a>
-          <a class="navbar-brand" href="#">About us</a>
-          <a class="navbar-brand" href="#">Services</a>
-          <a class="navbar-brand" href="#">For owners</a>
-  </nav>
+ <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="#">MDM Vets</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto w-100">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">For owners</a>
+      </li>
+    </ul>
+  </div>
+</nav>

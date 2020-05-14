6f2570d06123d3875b1af166db70c80ecd2d456d
mdm106 <mads.mcmahon@googlemail.com>
2020-05-14 12:20:53 +0100

    Owners controller updated with create(get) and createOwner(post) methods to deal with form view

--- app/Http/Controllers/Owners.php
+++ app/Http/Controllers/Owners.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Owner;
+use App\Http\Requests\OwnerRequest;
 
 class Owners extends Controller
 {
@@ -16,6 +17,24 @@ class Owners extends Controller
     public function show(Owner $owner)
     {
         return view("single_owner", ["owner" => $owner]);
+    }
+
+    public function create()
+    {
+        return view("form");
+    }
+
+    //accepts the request object to give access to the submitted data
+    public function createOwner(OwnerRequest $request)
+    {
+        //get all of the submitted data
+        $data = $request->all();
+
+        //create a new owner in the Owner database, passing in the submitted data
+        $owner = Owner::create($data);
+
+        //redirect the browser to the new owner
+        return redirect("/owners/{$owner->id}");
     }
 
 }

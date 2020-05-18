a031cede5958fc2104cc7f80db48fffdb2a4f673
mdm106 <mads.mcmahon@googlemail.com>
2020-05-18 17:03:45 +0100

    Adding validation to owners using a request

--- app/Http/Controllers/API/Owners.php
+++ app/Http/Controllers/API/Owners.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\API\OwnerRequest as Request;
 use App\Owner;
 
 class Owners extends Controller

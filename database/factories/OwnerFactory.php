d629d4b252ab578a119861e6c4b97e6af130c69c
mdm106 <mads.mcmahon@googlemail.com>
2020-05-20 10:54:13 +0100

    Adding owner factory'

---
+++ database/factories/OwnerFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Owner;
+use Faker\Generator as Faker;
+
+$factory->define(Owner::class, function (Faker $faker) {
+    return [
+        'first_name'=>$faker->firstName,
+        'last_name'=>$faker->lastName,
+        'telephone'=>$faker->mobileNumber,
+        'address_1'=>$faker->streetAddress,
+        'address_2'=>$faker->county,
+        'town'=>$faker->city,
+        'postcode'=>$faker->postcode,
+    ];
+});
+
+

b8cd8843d02bfc4d00edc1b9bf020855276ca7b8
mdm106 <mads.mcmahon@googlemail.com>
2020-05-20 14:54:55 +0100

    Adder method in adder class created using TDD

---
+++ tests/Unit/AdderTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use App\Adder;
+
+class AdderTest extends TestCase
+{
+    public function setUp() : void
+    {
+        $this->adder = new Adder();
+    }
+    
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testOnePlus0()
+    {
+        $this->assertSame(1, $this->adder->add(1, 0));
+    }
+
+    public function testOnePlusOne()
+    {
+        $this->assertSame(2, $this->adder->add(1, 1));
+    }   
+
+    public function testTenPlus5()
+    {
+        $this->assertSame(15, $this->adder->add(10, 5));
+    }
+
+}

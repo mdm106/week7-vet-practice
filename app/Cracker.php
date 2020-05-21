9f02a408304d0caf3bb0b725da4892a06fd33c5b
mdm106 <mads.mcmahon@googlemail.com>
2020-05-21 11:16:20 +0100

    Refactoring Cracker.php

--- app/Cracker.php
+++ app/Cracker.php
@@ -11,34 +11,30 @@ class Cracker
     public function __construct(string $key)
     {   
         $this->key = preg_split('//u', str_replace(' ', '', $key), -1, PREG_SPLIT_NO_EMPTY);
-        $this->alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+        $this->alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; //range('a', 'z) would be better
     }
 
 
-    public function translateWord(string $string) 
+    public function decrypt(string $string) 
     {
-        $split = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY); // string split that deal with unicodes
+        $split = preg_split('//u', $string, -1, PREG_SPLIT_NO_EMPTY); // string split that deal with unicodes - /mb_str_split
         $array = [];
         foreach ($split as $letter) {
-            array_push($array, array_search($letter, $this->key));
+            if ($letter === " ") {
+                array_push($array, " ");
+            } else {
+                array_push($array, array_search($letter, $this->key));
+            }
         }
         $answer = "";
         foreach ($array as $index) {
-            $answer .= $this->alphabet[$index];
+            if($index === " ") {
+                $answer .= " ";
+            } else {
+                $answer .= $this->alphabet[$index];
+            }
         }
         return $answer;
     }
     
-    
-    public function decrypt(string $string)
-    {   
-        $words = explode(' ', $string);
-        $final = "";
-        foreach ($words as $word) {
-            $final .= $this->translateWord($word);
-            $final .= " ";
-        }
-        return rtrim($final);
-        
-    }
 }
